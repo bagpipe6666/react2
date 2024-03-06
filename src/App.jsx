bea13caf3b0793657599f518146b2cbbd0a52532
bagpipe6666 <bumpoloyou@gmail.com>
2024-03-06 09:19:47 +0700

    up

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import From from "./assets/From";
+
+function App(){
+    return (
+        <div>
+            <h1>App From</h1>
+            <From />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/assets/From.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import ShowName from "./ShowName";
+
+export default function From() {
+    const [name, setName] = useState("");
+    const [displayName, setDisplayName]= useState(false);
+
+    const handleChange = (event) => {
+        console.log(event);
+        setName(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert("A name was submitted:" + name);
+        console.log("A name was submitted:" + name);
+
+        name.length >= 5 ? setDisplayName(true) : setDisplayName(false);
+
+    };
+    return(
+        <div>
+            <h1>From to input data</h1>
+            <form>
+                <label>
+                    Name : 
+                    <input type="text" value={name} onChange={handleChange} />
+                </label>
+                <input type="submit" value="Submit" onChange={handleSubmit} />
+            </form>
+            <ShowName name={name} show={displayName} />
+        </div>
+    );
+}
